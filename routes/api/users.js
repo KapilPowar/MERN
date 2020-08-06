b3d4ab6b3b66cda9d417c80bd8ac96fd23a6e5b3
kapil powar <kapil.powar@in.ibm.com>
2020-08-06 19:42:52 +0530

    JWT token

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const {check, validationResult} = require('express-validator/check');
+
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+const {check, validationResult} = require('express-validator');
+
+const User = require('../../models/User');
+
 //@route    GET api/users
 //@desc     Test route
 //@access   Public
@@ -14,13 +23,53 @@ router.post('/', [
     check('email', 'Please include valid email').isEmail(),
     check('password','Please Enter valid password with 6 or more characters').isLength({min: 6})
 ],
-(req, res) => {
+async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()){
         return res.status(400).json({ errors : errors.array() });
     }
+    
+    const { name ,email, password} = req.body;
+    try {
+        
+        let user = await User.findOne({ email});
+        if (user){
+           return res.status(400).json({errors : [{ msg : 'user already exists' }] });
+        }
 
-    res.send('get Users route');
+        const avatar = gravatar.url(email, { 
+                s:'200',r:'pg', d:'mm'
+        });
+
+        user = new User({
+            name, email, avatar, password
+        });
+
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(password,salt);
+
+        await user.save();
+
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+        jwt.sign(
+            payload,
+            config.get('jwtSecret'),
+            { expiresIn : 3600 } ,
+            (err, token) => {
+                if (err) throw err;
+                res.json({token});
+            }
+        );
+       
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+    
 });
 
 module.exports = router;
